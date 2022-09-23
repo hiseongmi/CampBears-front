542a46eabe661bd2231135027458ab659ca6334c
calf <81305023+qhaud98@users.noreply.github.com>
2022-09-23 23:12:41 +0900

    custom input 완료

---
+++ src/components/customInput.vue
@@ -0,0 +1,33 @@
+<script>
+import { computed, ref } from "vue";
+
+export default {
+  name: "customInput",
+  props: {
+    placeholder: {
+      type: String,
+      required: false
+    },
+    model: {
+      type: String,
+      required: false
+    },
+    customClass: {
+      type: String,
+      required: false
+    }
+  },
+  setup() {
+    return {};
+  },
+  methods: {
+    handleInput(e) {
+      this.$emit("modalValue", e.target.value);
+    }
+  }
+};
+</script>
+<template>
+  <input :class="customClass" class="custom-input" type="text" :placeholder="placeholder" @input="handleInput">
+  <slot name="element"></slot>
+</template>
--- src/views/mainPage.vue
+++ src/views/mainPage.vue
@@ -1,10 +1,39 @@
 <script>
+import customInput from "../components/customInput.vue";
+import { ref, watch } from "vue";
+
 export default {
-	name: 'mainPage',
+  name: "mainPage",
+  components: {
+    customInput
+  },
+  setup() {
+    const inputValue = ref();
+
+    const updateValue = (event) => {
+      inputValue.value = event;
+      console.log(inputValue.value);
+    };
+
+
+    return {
+      updateValue,
+      inputValue
+    };
+  }
 };
 </script>
 <template>
-	<div>main page</div>
-	<div>여기에 작업하면 됩니다.</div>
-
+  <!--  custom input 사용법-->
+  <custom-input placeholder="hi" @modalValue="updateValue" :custom-class="`test-input`">
+    <template v-slot:element>icon</template>
+  </custom-input>
+  {{ inputValue }}
 </template>
+
+<style lang="scss">
+.test-input {
+  width: 100px;
+  height: 100px;
+}
+</style>
