aea345c0e8837b8636b99eabd4879a322ca39b1a
calf <81305023+qhaud98@users.noreply.github.com>
2022-09-27 23:11:29 +0900

    store 수정

--- src/main.js
+++ src/main.js
@@ -3,5 +3,6 @@ import { createApp } from 'vue';
 // import '../src/assets/css/app.css.map';
 import App from './App.vue';
 import router from './router';
+import store from './store';
 
-createApp(App).use(router).mount('#app');
+createApp(App).use(router).use(store).mount('#app');
