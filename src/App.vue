a48649b09ff03ad94edbf33233dfbf8e724a9446
hcl0ud <148cl0ud@gmail.com>
2022-10-07 00:16:26 +0900

    add to [onUnmounted]

--- src/App.vue
+++ src/App.vue
@@ -1,48 +1,52 @@
 <script>
 import { useRoute } from "vue-router";
 import customLoading from "./components/layout/customLoading.vue";
-import { onMounted, ref } from "vue";
+import { onMounted, onUnmounted, ref } from "vue";
 import { CONSTANTS } from "./constants.js";
 import { apiClient } from "./utils/axios.js";
 
 export default {
-  name: "App",
-  components: {
-    customLoading
-  },
-  setup() {
-    const route = useRoute();
-    const isLoading = ref(false);
+	name: "App",
+	components: {
+		customLoading
+	},
+	setup() {
+		const route = useRoute();
+		const isLoading = ref(false);
 
-    const eventHandler = (e) => {
-      if (e.detail === CONSTANTS.KEY_LIST.EVENT_MESSAGE.PROCESS) {
-        isLoading.value = true;
-      } else if (e.detail === CONSTANTS.KEY_LIST.EVENT_MESSAGE.COMPLETE) {
-        isLoading.value = false;
-      }
-    };
+		const eventHandler = (e) => {
+			if (e.detail === CONSTANTS.KEY_LIST.EVENT_MESSAGE.PROCESS) {
+				isLoading.value = true;
+			} else if (e.detail === CONSTANTS.KEY_LIST.EVENT_MESSAGE.COMPLETE) {
+				isLoading.value = false;
+			}
+		};
 
-    const getAPI = async () => {
-      const data = await apiClient("/pbl/getProductList", {});
-    };
+		const getAPI = async () => {
+			const data = await apiClient("/pbl/getProductList", {});
+		};
 
-    onMounted(() => {
-      addEventListener(CONSTANTS.KEY_LIST.EVENT_LIST.LOADING, eventHandler);
-      console.warn("start");
-      // getAPI();
-    });
+		onMounted(() => {
+			addEventListener(CONSTANTS.KEY_LIST.EVENT_LIST.LOADING, eventHandler);
+			console.warn("start");
+			// getAPI();
+		});
 
-    return {
-      isLoading
-    };
+		onUnmounted(() => {
+			removeEventListener(CONSTANTS.KEY_LIST.EVENT_LIST.LOADING, eventHandler)
+		})
 
-  }
+		return {
+			isLoading
+		};
+
+	}
 };
 </script>
 
 <template>
-  <router-view />
-  <custom-loading v-if="isLoading"></custom-loading>
+	<router-view />
+	<custom-loading v-if="isLoading"></custom-loading>
 </template>
 
 <style src="./assets/css/app.css">
