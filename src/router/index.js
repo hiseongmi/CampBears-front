58bd217c6b8956440370ee88e126803c460597b9
calf <81305023+qhaud98@users.noreply.github.com>
2022-10-21 19:24:46 +0900

    router 정리

--- src/router/index.js
+++ src/router/index.js
@@ -7,54 +7,19 @@ const routes = [
 		component: () => import("../views/mainPage.vue"),
 	},
 	{
-		path: "/loginPage",
-		name: "loginPage",
-		component: () => import("../components/yd/loginPage.vue"),
-	},
-	{
-		path: "/signupPage",
-		name: "signupPage",
-		component: () => import("../components/yd/signupPage.vue"),
-	},
-	{
 		path: "/samplePage",
 		name: "samplePage",
 		component: () => import("../views/samplePage.vue"),
 	},
 	{
-		path: "/sample-login",
-		name: "sample-login",
-		component: () => import("../views/sample-login.vue"),
-	},
-	{
-		path: "/newsPage",
-		name: "newsPage",
-		component: () => import("../components/sungmi/newsPage.vue"),
-	},
-	{
-		path: "/PostPage",
-		name: "PostPage",
-		component: () => import("../components/sungmi/PostPage.vue"),
-	},
-	{
-		path: "/detailScreenPopup/:id",
-		name: "detailScreenPopup",
-		component: () => import("../components/manager/popups/detailScreenPopup.vue"),
-	},
-	{
-		path: "/auth",
-		name: "loginAccess",
-		component: () => import("../components/snslogin/loginAccess.vue"),
-	},
-	{
-		path: "/loginNaver",
-		name: "loginNaver",
-		component: () => import("../components/snslogin/loginNaver.vue"),
-	},
-	{
 		path: "/myPage",
 		name: "myPage",
 		component: () => import("../views/myPage.vue"),
+	},
+	{
+		path: "/login",
+		name: "myPage",
+		component: () => import("../views/login.vue"),
 	},
 ];
 
