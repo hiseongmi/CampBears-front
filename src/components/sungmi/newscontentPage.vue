8bc937107eba07dc2d92aa216046c19b96ea73ba
hcl0ud <148cl0ud@gmail.com>
2022-10-21 20:24:15 +0900

    mypage 초안

--- src/components/sungmi/newscontentPage.vue
+++ src/components/sungmi/newscontentPage.vue
@@ -1,32 +1,31 @@
 <script>
 import Profile from "./profile.vue";
 import postImage from "../../data/postImage.js";
-import store, {POPUP_TYPE, STORE_TYPE} from "../../store/index.js";
+import store, { POPUP_TYPE, STORE_TYPE } from "../../store/index.js";
 
 export default {
-  name: "newsContentPage",
-  components: {
-    Profile,
-  },
-  setup() {
-    const openDetail = () => {
-      store.commit(STORE_TYPE.popupType, POPUP_TYPE.DETAIL_SCREEN)
-    }
-    return {
-      openDetail,
-      postImage
-    }
-  }
-
-}
+	name: "newsContentPage",
+	components: {
+		Profile,
+	},
+	setup() {
+		const openDetail = () => {
+			store.commit(STORE_TYPE.popupType, POPUP_TYPE.DETAIL_SCREEN);
+		};
+		return {
+			openDetail,
+			postImage,
+		};
+	},
+};
 </script>
 <template>
-  <div class="news-ul">
-    <div class="news-ul-li" :key="id" v-for="(item,id) in postImage">
-      <div class="news-ul-li-write" @click="openDetail(item.id)">
-        <profile></profile>
-        <img :src="item.img" alt="Posts"/>
-      </div>
-    </div>
-  </div>
+	<div class="news-ul">
+		<div class="news-ul-li" :key="id" v-for="(item, id) in postImage">
+			<div class="news-ul-li-write" @click="openDetail(item.id)">
+				<!--        <profile></profile>-->
+				<img :src="item.img" alt="Posts" />
+			</div>
+		</div>
+	</div>
 </template>
--- src/views/myPage.vue
+++ src/views/myPage.vue
@@ -2,10 +2,12 @@
 import customButton from "../components/layout/customButton.vue";
 import customInput from "../components/layout/customInput.vue";
 import { ref } from "vue";
+import NewsContentPage from "../components/sungmi/newscontentPage.vue";
 
 export default {
 	name: "myPage",
 	components: {
+		NewsContentPage,
 		customButton,
 		customInput,
 	},
@@ -22,9 +24,21 @@ export default {
 </script>
 
 <template>
-	<custom-button :placeholder="'피드'" :onClick="clickButton" />
-	<custom-button :placeholder="'판매'" :onClick="clickButton" />
-	<custom-button :placeholder="'대여'" :onClick="clickButton" />
-	<custom-button :placeholder="'후기'" :onClick="clickButton" />
-	<custom-button :placeholder="'찜 목록'" :onClick="clickButton" />
+	<div class="myPage">
+		<div class="myProfile">
+			<img src="assets/image/camping.png" alt="" />
+		</div>
+		<div class="myContents">
+			<div class="tapBar">
+				<custom-button :placeholder="'피드'" :onClick="clickButton" />
+				<custom-button :placeholder="'판매'" :onClick="clickButton" />
+				<custom-button :placeholder="'대여'" :onClick="clickButton" />
+				<custom-button :placeholder="'후기'" :onClick="clickButton" />
+				<custom-button :placeholder="'찜 목록'" :onClick="clickButton" />
+			</div>
+			<div class="myFeed">
+				<news-content-page />
+			</div>
+		</div>
+	</div>
 </template>
