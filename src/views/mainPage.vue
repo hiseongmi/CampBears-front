6e03428a9bc833fe3097f261cd4dbbe8cd71d04d
youngDuck <92674381+qaehreh@users.noreply.github.com>
2022-10-24 04:19:26 +0900

    Add main page main image & add top menu icon (modifying main image size)

--- src/views/mainPage.vue
+++ src/views/mainPage.vue
@@ -21,9 +21,253 @@ export default {
 </script>
 <template>
   <div class="main">
-    <a><img src="/assets/image/mainpage2.png" alt="캠핑장찾기"></a>
-    <!--  </div>-->
-    <!--  <div class="main">-->
-    <a><img src="/assets/image/mainpage3.png" alt="캠핑장찾기"></a>
+    <a href="">awfwaf</a>
   </div>
+  <div class="main-sec">
+    <a href="">wfawfaf</a>
+  </div>
+  <div class="main-icon">
+    <a href="">
+      <svg width="68" height="68" viewBox="0 0 68 68" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <g filter="url(#filter0_d_1_2)">
+          <rect x="2" width="60" height="60" rx="10" fill="white"/>
+          <rect x="2.5" y="0.5" width="59" height="59" rx="9.5" stroke="#DBDBDB"/>
+        </g>
+        <path
+            d="M37.5277 18.3964H37.5277V18.3937C37.5277 15.4638 39.7699 13.0396 42.3216 11.6552L42.3216 11.6552C42.7768 11.4083 43.0292 11.503 43.1538 11.6153C43.3118 11.7579 43.461 12.0994 43.3778 12.6574L43.3773 12.6605C43.3684 12.7231 43.3538 12.8 43.3351 12.8986C43.3259 12.9474 43.3156 13.0015 43.3046 13.0618C43.3014 13.0782 43.2981 13.095 43.2947 13.1122C43.2314 13.4363 43.1429 13.8892 43.1429 14.1717C43.1429 18.2427 46.3864 21.5499 50.4677 21.5499H50.4723C50.5351 21.5499 50.6088 21.5499 50.6844 21.5466C51.2807 21.5309 51.6159 21.6961 51.7359 21.8395C51.7855 21.8988 51.8099 21.9646 51.8038 22.0561C51.797 22.1585 51.7486 22.3203 51.5923 22.5403C50.653 23.8623 49.4748 24.9064 48.0618 25.4818L47.3361 23.632C47.121 23.0635 46.7374 22.5743 46.2365 22.2298C45.7351 21.8849 45.1402 21.7016 44.5317 21.7045H38.2865C37.782 20.6764 37.5217 19.5442 37.5277 18.3964Z"
+            fill="#FA6C1A" stroke="#2F2E2C"/>
+        <path d="M10 49.2667L19.6336 24.9152H44.3865L54.1538 49.2667H28.9995L21.373 29.7319V49.2667H10Z"
+              stroke="#2F2E2C"
+              stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+        <defs>
+          <filter id="filter0_d_1_2" x="0" y="0" width="68" height="68" filterUnits="userSpaceOnUse"
+                  color-interpolation-filters="sRGB">
+            <feFlood flood-opacity="0" result="BackgroundImageFix"/>
+            <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+                           result="hardAlpha"/>
+            <feOffset dx="2" dy="4"/>
+            <feGaussianBlur stdDeviation="2"/>
+            <feComposite in2="hardAlpha" operator="out"/>
+            <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"/>
+            <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_1_2"/>
+            <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_1_2" result="shape"/>
+
+          </filter>
+        </defs>
+      </svg>
+      <span>카라반</span>
+    </a>
+
+
+    <a href="">
+      <svg width="68" height="68" viewBox="0 0 68 68" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <g filter="url(#filter0_d_1_2)">
+          <rect x="2" width="60" height="60" rx="10" fill="white"/>
+          <rect x="2.5" y="0.5" width="59" height="59" rx="9.5" stroke="#DBDBDB"/>
+        </g>
+        <path
+            d="M57 43C57.5523 43 58 42.5523 58 42C58 41.4477 57.5523 41 57 41L57 43ZM37 41L36 41L36 43L37 43L37 41ZM57 41L37 41L37 43L57 43L57 41Z"
+            fill="#2F2E2C"/>
+        <path
+            d="M41 42V43H43V42H41ZM42 22H43V21H42V22ZM34 22V21H33V22H34ZM33 32.9524V33.9524H35V32.9524H33ZM43 42V22H41V42H43ZM42 21H34V23H42V21ZM33 22V32.9524H35V22H33Z"
+            fill="#2F2E2C"/>
+        <path
+            d="M52 42.0462V27.2615L45.1383 14.3846C44.6809 13.5897 43.3085 12 41.4787 12H12.2021C11.1348 12 9 12.5723 9 14.8615V40.6154C9 41.4103 9.45745 43 11.2872 43H14.9468"
+            stroke="#2F2E2C" stroke-width="2" stroke-linecap="square" stroke-linejoin="round"/>
+        <rect x="16" y="22" width="12" height="8" fill="#FA6C1A" stroke="#2F2E2C" stroke-width="2"/>
+        <circle cx="25.5" cy="43.5" r="5.5" stroke="#2F2E2C" stroke-width="2"/>
+        <defs>
+          <filter id="filter0_d_1_2" x="0" y="0" width="68" height="68" filterUnits="userSpaceOnUse"
+                  color-interpolation-filters="sRGB">
+            <feFlood flood-opacity="0" result="BackgroundImageFix"/>
+            <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+                           result="hardAlpha"/>
+            <feOffset dx="2" dy="4"/>
+            <feGaussianBlur stdDeviation="2"/>
+            <feComposite in2="hardAlpha" operator="out"/>
+            <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"/>
+            <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_1_2"/>
+            <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_1_2" result="shape"/>
+          </filter>
+        </defs>
+      </svg>
+      <span>카라반</span>
+    </a>
+
+    <a href="">
+      <svg width="68" height="68" viewBox="0 0 68 68" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <g filter="url(#filter0_d_1_2)">
+          <rect x="2" width="60" height="60" rx="10" fill="white"/>
+          <rect x="2.5" y="0.5" width="59" height="59" rx="9.5" stroke="#DBDBDB"/>
+        </g>
+        <path
+            d="M32.4176 29.6741C30.1111 35.6205 25.1686 44.5402 22.2031 49H42.9617C39.5019 44.5402 34.8889 35.6205 32.4176 29.6741Z"
+            fill="#FA6C1A"/>
+        <path
+            d="M32.5824 12L33.4044 11.4306C33.2281 11.176 32.9438 11.0175 32.6346 11.0014C32.3253 10.9852 32.026 11.1132 31.8241 11.348L32.5824 12ZM17.59 23.8929L17.1212 23.0096C16.8374 23.1602 16.6433 23.438 16.5994 23.7563L17.59 23.8929ZM14.1303 49L13.1396 48.8635C13.1002 49.1498 13.1865 49.4392 13.3764 49.6571C13.5664 49.8749 13.8412 50 14.1303 50V49ZM51.0345 49V50C51.3237 50 51.5987 49.8748 51.7886 49.6567C51.9785 49.4387 52.0647 49.149 52.025 48.8626L51.0345 49ZM47.5747 24.058L48.5652 23.9206C48.5223 23.6111 48.3371 23.3394 48.0647 23.1863L47.5747 24.058ZM11 48C10.4477 48 10 48.4477 10 49C10 49.5523 10.4477 50 11 50V48ZM54 50C54.5523 50 55 49.5523 55 49C55 48.4477 54.5523 48 54 48V50ZM22.2031 49L21.3704 48.4463C21.1663 48.7532 21.1476 49.1475 21.3217 49.4724C21.4958 49.7972 21.8345 50 22.2031 50V49ZM32.4176 29.6741L33.3411 29.2903C33.1843 28.9132 32.814 28.6693 32.4057 28.6742C31.9973 28.679 31.633 28.9317 31.4853 29.3125L32.4176 29.6741ZM42.9617 49V50C43.3434 50 43.6918 49.7827 43.8597 49.4399C44.0276 49.0971 43.9858 48.6886 43.7518 48.3871L42.9617 49ZM31.8241 11.348C29.1513 14.4566 22.6021 20.1004 17.1212 23.0096L18.0589 24.7761C23.7811 21.7389 30.5218 15.9303 33.3406 12.652L31.8241 11.348ZM16.5994 23.7563L13.1396 48.8635L15.1209 49.1365L18.5807 24.0294L16.5994 23.7563ZM14.1303 50H51.0345V48H14.1303V50ZM52.025 48.8626L48.5652 23.9206L46.5842 24.1954L50.044 49.1374L52.025 48.8626ZM48.0647 23.1863C46.2872 22.1873 43.4003 20.3858 40.5555 18.2494C37.6921 16.099 34.9571 13.6722 33.4044 11.4306L31.7603 12.5694C33.5027 15.085 36.4516 17.6686 39.3545 19.8486C42.276 22.0426 45.2377 23.8916 47.0847 24.9298L48.0647 23.1863ZM11 50H54V48H11V50ZM23.0358 49.5537C26.0302 45.0504 31.0109 36.0661 33.3499 30.0357L31.4853 29.3125C29.2113 35.175 24.307 44.0299 21.3704 48.4463L23.0358 49.5537ZM31.4942 30.0579C33.9795 36.0382 38.6327 45.0512 42.1716 49.6129L43.7518 48.3871C40.3711 44.0292 35.7982 35.2029 33.3411 29.2903L31.4942 30.0579ZM42.9617 48H22.2031V50H42.9617V48Z"
+            fill="#2F2E2C"/>
+        <defs>
+          <filter id="filter0_d_1_2" x="0" y="0" width="68" height="68" filterUnits="userSpaceOnUse"
+                  color-interpolation-filters="sRGB">
+            <feFlood flood-opacity="0" result="BackgroundImageFix"/>
+            <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+                           result="hardAlpha"/>
+            <feOffset dx="2" dy="4"/>
+            <feGaussianBlur stdDeviation="2"/>
+            <feComposite in2="hardAlpha" operator="out"/>
+            <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"/>
+            <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_1_2"/>
+            <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_1_2" result="shape"/>
+          </filter>
+        </defs>
+      </svg>
+      <span>글램핑</span>
+    </a>
+
+    <a href="">
+      <svg width="68" height="68" viewBox="0 0 68 68" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <g filter="url(#filter0_d_1_2)">
+          <rect x="2" width="60" height="60" rx="10" fill="white"/>
+          <rect x="2.5" y="0.5" width="59" height="59" rx="9.5" stroke="#DBDBDB"/>
+        </g>
+        <path
+            d="M16 25L18.125 15.7917C18.4792 14.5278 20.0375 12 23.4375 12H42.0312C45.75 12 47.3438 14.1667 47.875 15.7917L50 25"
+            stroke="#2F2E2C" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+        <path
+            d="M50.2973 25H15.7027C13.5405 25 13 27.6078 13 28.9118V41.2059C13 43.4412 15.1622 44 16.2432 44H49.7568C52.3513 44 53 42.1373 53 41.2059V28.9118C53 25.7824 51.1982 25 50.2973 25Z"
+            stroke="#2F2E2C" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+        <path
+            d="M17.5 47C17.5 48.3807 18.6193 49.5 20 49.5C21.3807 49.5 22.5 48.3807 22.5 47L17.5 47ZM22.5 46L22.5 43.5L17.5 43.5L17.5 46L22.5 46ZM22.5 47L22.5 46L17.5 46L17.5 47L22.5 47Z"
+            fill="#2F2E2C"/>
+        <path
+            d="M43.5 47C43.5 48.3807 44.6193 49.5 46 49.5C47.3807 49.5 48.5 48.3807 48.5 47L43.5 47ZM48.5 46L48.5 43.5L43.5 43.5L43.5 46L48.5 46ZM48.5 47L48.5 46L43.5 46L43.5 47L48.5 47Z"
+            fill="#2F2E2C"/>
+        <path
+            d="M15 23H17V19H15V23ZM14 19C12.8954 19 12 19.8954 12 21C12 22.1046 12.8954 23 14 23V19ZM15 19H14.5V23H15V19ZM14.5 19H14V23H14.5V19Z"
+            fill="#2F2E2C"/>
+        <path
+            d="M51 19L49 19L49 23L51 23L51 19ZM52 23C53.1046 23 54 22.1046 54 21C54 19.8954 53.1046 19 52 19L52 23ZM51 23L51.5 23L51.5 19L51 19L51 23ZM51.5 23L52 23L52 19L51.5 19L51.5 23Z"
+            fill="#2F2E2C"/>
+        <circle cx="23.3635" cy="34.3636" r="2.22727" fill="#FA6C1A" stroke="#2F2E2C"/>
+        <circle cx="44.091" cy="34.3636" r="2.22727" fill="#FA6C1A" stroke="#2F2E2C"/>
+        <defs>
+          <filter id="filter0_d_1_2" x="0" y="0" width="68" height="68" filterUnits="userSpaceOnUse"
+                  color-interpolation-filters="sRGB">
+            <feFlood flood-opacity="0" result="BackgroundImageFix"/>
+            <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+                           result="hardAlpha"/>
+            <feOffset dx="2" dy="4"/>
+            <feGaussianBlur stdDeviation="2"/>
+            <feComposite in2="hardAlpha" operator="out"/>
+            <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"/>
+            <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_1_2"/>
+            <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_1_2" result="shape"/>
+          </filter>
+        </defs>
+      </svg>
+      <span>오토캠핑</span>
+    </a>
+
+    <a href="">
+      <svg width="68" height="68" viewBox="0 0 68 68" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <g filter="url(#filter0_d_1_2)">
+          <rect x="2" width="60" height="60" rx="10" fill="white"/>
+          <rect x="2.5" y="0.5" width="59" height="59" rx="9.5" stroke="#DBDBDB"/>
+        </g>
+        <path
+            d="M29.5 20L27.5 30C27.1667 32.5 25 37.6 19 38H13.5L14 26.5C14 22 15.5 12.5 25.5 12.5H36C36.8333 12.5 38.8 12.9 40 14.5L45 22L54 23.5V26L52.5 34C52 36 50.5 37.5 48 38C45.6466 38.4707 39.6667 39 37 39.5V50"
+            stroke="#2F2E2C" stroke-width="2"/>
+        <circle cx="36.7273" cy="20.7273" r="2.22727" fill="#FA6C1A" stroke="#2F2E2C"/>
+        <defs>
+          <filter id="filter0_d_1_2" x="0" y="0" width="68" height="68" filterUnits="userSpaceOnUse"
+                  color-interpolation-filters="sRGB">
+            <feFlood flood-opacity="0" result="BackgroundImageFix"/>
+            <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+                           result="hardAlpha"/>
+            <feOffset dx="2" dy="4"/>
+            <feGaussianBlur stdDeviation="2"/>
+            <feComposite in2="hardAlpha" operator="out"/>
+            <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"/>
+            <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_1_2"/>
+            <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_1_2" result="shape"/>
+          </filter>
+        </defs>
+      </svg>
+      <span>반려동물</span>
+    </a>
+
+    <a href="">
+      <svg width="68" height="68" viewBox="0 0 68 68" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <g filter="url(#filter0_d_1_2)">
+          <rect x="2" width="60" height="60" rx="10" fill="white"/>
+          <rect x="2.5" y="0.5" width="59" height="59" rx="9.5" stroke="#DBDBDB"/>
+        </g>
+        <path
+            d="M32.3149 12.3258C42.8857 12.3258 48.0452 20.191 49.6182 24.5955C55.9104 24.5955 56.5396 28.6854 56.5396 31.2022C56.5396 35.7325 51.7156 37.0749 49.3036 37.1797C44.8991 47.2472 37.034 49.1348 32.0003 49.1348C21.1778 49.1348 15.7456 40.8501 14.3823 36.8651C8.84521 36.8651 7.46094 33.1947 7.46094 31.2022C7.46094 25.4134 12.18 24.5955 14.3823 24.5955C19.7306 13.5842 26.9666 12.3258 32.3149 12.3258Z"
+            stroke="#2F2E2C" stroke-width="2"/>
+        <path
+            d="M22.875 26.875H26.25V30.25H22.875V26.875ZM37.75 26.875H41.125V30.25H37.75V26.875ZM35.1571 41.872C34.1562 42.2866 33.0834 42.5 32 42.5C29.812 42.5 27.7135 41.6308 26.1664 40.0836C25.4003 39.3175 24.7926 38.4081 24.378 37.4071C24.0273 36.5605 23.8206 35.6624 23.7652 34.75H40.2348C40.1794 35.6624 39.9727 36.5605 39.622 37.4071C39.2074 38.4081 38.5997 39.3175 37.8336 40.0836C37.0675 40.8497 36.1581 41.4574 35.1571 41.872Z"
+            fill="#FA6C1A" stroke="#2F2E2C"/>
+        <rect x="23" y="27" width="3" height="3" fill="#2F2E2C"/>
+        <rect x="38" y="27" width="3" height="3" fill="#2F2E2C"/>
+        <path d="M32.2 11.4L31.6 12.2L33.2 13.4L33.8 12.6L32.2 11.4ZM33.8 12.6L36.8 8.6L35.2 7.4L32.2 11.4L33.8 12.6Z"
+              fill="#2F2E2C"/>
+        <defs>
+          <filter id="filter0_d_1_2" x="0" y="0" width="68" height="68" filterUnits="userSpaceOnUse"
+                  color-interpolation-filters="sRGB">
+            <feFlood flood-opacity="0" result="BackgroundImageFix"/>
+            <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+                           result="hardAlpha"/>
+            <feOffset dx="2" dy="4"/>
+            <feGaussianBlur stdDeviation="2"/>
+            <feComposite in2="hardAlpha" operator="out"/>
+            <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"/>
+            <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_1_2"/>
+            <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_1_2" result="shape"/>
+          </filter>
+        </defs>
+      </svg>
+      <span>키즈</span>
+    </a>
+
+    <a href="">
+      <svg width="68" height="68" viewBox="0 0 68 68" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <g filter="url(#filter0_d_1_2)">
+          <rect x="2" width="60" height="60" rx="10" fill="white"/>
+          <rect x="2.5" y="0.5" width="59" height="59" rx="9.5" stroke="#DBDBDB"/>
+        </g>
+        <path d="M31.6361 13.3636L20.7271 25H43.2725L31.6361 13.3636Z" stroke="#2F2E2C" stroke-width="3"
+              stroke-linecap="round" stroke-linejoin="round"/>
+        <path
+            d="M51.6172 30H11.9647H10.1118C9.22235 30 9 30.9212 9 31.3818L11.2235 44.5091C12.4094 47.5491 15.1764 48.7697 16.4117 49H47.1702C50.7278 49 52.3583 46.0061 52.7289 44.5091L54.9524 31.3818C55.2489 30.2764 54.0877 30 53.4701 30H51.6172Z"
+            stroke="#2F2E2C" stroke-width="2"/>
+        <path d="M8 25L56 25" stroke="#FA6C1A" stroke-width="4" stroke-linecap="round" stroke-linejoin="round"/>
+        <defs>
+          <filter id="filter0_d_1_2" x="0" y="0" width="68" height="68" filterUnits="userSpaceOnUse"
+                  color-interpolation-filters="sRGB">
+            <feFlood flood-opacity="0" result="BackgroundImageFix"/>
+            <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+                           result="hardAlpha"/>
+            <feOffset dx="2" dy="4"/>
+            <feGaussianBlur stdDeviation="2"/>
+            <feComposite in2="hardAlpha" operator="out"/>
+            <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"/>
+            <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_1_2"/>
+            <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_1_2" result="shape"/>
+          </filter>
+        </defs>
+      </svg>
+      <span>캠프닉</span>
+    </a>
+  </div>
+  <!--  <div class="icon-name">-->
+  <!--    <span>카라반</span>-->
+  <!--    <span>카라반</span>-->
+  <!--    <span>카라반</span>-->
+  <!--    <span>카라반</span>-->
+  <!--    <span>카라반</span>-->
+  <!--    <span>카라반</span>-->
+  <!--    <span>카라반</span>-->
+
+  <!--  </div>-->
 </template>
