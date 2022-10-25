6fcbc830188ab5e13478c365923a409c2c09b8de
hcl0ud <148cl0ud@gmail.com>
2022-10-25 20:35:35 +0900

    mypage componentChange error

--- src/views/myPage.vue
+++ src/views/myPage.vue
@@ -1,7 +1,7 @@
 <script>
 import customButton from "../components/layout/customButton.vue";
 import customInput from "../components/layout/customInput.vue";
-import {ref} from "vue";
+import { ref } from "vue";
 import NewsContentPage from "../components/snsBoard/snsContentPage.vue";
 
 export default {
@@ -12,12 +12,13 @@ export default {
     customInput,
   },
   setup() {
-    const clickButton = () => {
-      console.log("응애");
+    let cc = ref(0);
+    const componentChange = page => {
+      console.log(page);
     };
-
     return {
-      clickButton,
+      cc,
+      componentChange,
     };
   },
 };
@@ -26,21 +27,21 @@ export default {
 <template>
   <div class="myPage">
     <div class="myProfile">
-      <img class="backgroundPicture" src="/assets/image/camping.png" alt=""/>
-      <img class="profilePicture" src="/assets/image/bgs.png" alt=""/>
-      불멍온도
+      <img class="backgroundPicture" src="/assets/image/camping.png" alt="" />
+      <img class="profilePicture" src="/assets/image/bgs.png" alt="" />
     </div>
     <div class="myContents">
       <div class="tapBar">
-        <custom-button :placeholder="'피드'" :onClick="clickButton"/>
-        <custom-button :placeholder="'판매'" :onClick="clickButton"/>
-        <custom-button :placeholder="'대여'" :onClick="clickButton"/>
-        <custom-button :placeholder="'저장'" :onClick="clickButton"/>
-        <custom-button :placeholder="'후기'" :onClick="clickButton"/>
+        <custom-button :placeholder="'피드'" :onClick="componentChange(1)" />
+        <custom-button :placeholder="'판매'" :onClick="componentChange(2)" />
+        <custom-button :placeholder="'대여'" :onClick="componentChange(3)" />
+        <custom-button :placeholder="'저장'" :onClick="componentChange(4)" />
+        <custom-button :placeholder="'후기'" :onClick="componentChange(5)" />
       </div>
-      <div class="myFeed">
-        <news-content-page/>
+      <div v-if="cc === 1" class="myFeed">
+        <news-content-page />
       </div>
+      <custom-input v-else-if="cc === 5"></custom-input>
     </div>
   </div>
 </template>
