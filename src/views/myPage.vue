cd5ed164392d899ae441459c0866eeddd1b712aa
hcl0ud <148cl0ud@gmail.com>
2022-10-31 17:23:00 +0900

    mypage - profile edit page

--- src/views/myPage.vue
+++ src/views/myPage.vue
@@ -3,10 +3,12 @@ import customButton from "../components/layout/customButton.vue";
 import customInput from "../components/layout/customInput.vue";
 import { ref } from "vue";
 import snsContentPage from "../components/snsBoard/snsContentPage.vue";
+import UpdateProfile from "../components/mypage/updateProfile.vue";
 
 export default {
   name: "myPage",
   components: {
+    UpdateProfile,
     snsContentPage,
     customButton,
     customInput,
@@ -18,20 +20,26 @@ export default {
       RENT: "rent",
       SAVE: "save",
       REVIEW: "review",
-    };
-    const profileIntroduce = {
-      temperature: "100.0",
-      nickName: "베어 물어쓰",
-      introduce: "캠핑을 좋아하는 30대 아저씨에요^^",
-    };
+      EDIT: "profileUpdate",
+    }; // 컴포넌트 이름
+
     const tabIndex = ref(tabType.FEED);
 
     const componentChange = v => {
       console.log(v);
       tabIndex.value = v;
     };
+
+    const profileInfo = ref({
+      backgroundPicture: "/assets/image/camping.png",
+      profilePicture: "/assets/image/bgs.png",
+      temperature: "100.0",
+      nickName: "베어 물어쓰",
+      introduce: "캠핑을 좋아하는 30대 아저씨에요^^",
+    }); // 소개에 띄워줄 내용
+
     return {
-      profileIntroduce,
+      profileInfo,
       tabIndex,
       tabType,
       componentChange,
@@ -43,12 +51,12 @@ export default {
 <template>
   <div class="myPage">
     <div class="myProfile">
-      <img class="backgroundPicture" src="/assets/image/camping.png" alt="" />
-      <img class="profilePicture" src="/assets/image/bgs.png" alt="" />
+      <img class="backgroundPicture" :src="profileInfo.backgroundPicture" alt="" />
+      <img class="profilePicture" :src="profileInfo.profilePicture" alt="" />
       <div class="profileIntroduce">
         <span class="temperature">
           불멍온도
-          {{ profileIntroduce.temperature }}
+          {{ profileInfo.temperature }}
           <svg width="12" height="13" viewBox="0 0 10 15" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path
               d="M3.5558 0.157967C3.72991 -0.0559007 4 -0.052971 4.17411 0.160896C4.79018 0.919685 5.3683 1.73707 5.90848 2.62183C6.15402 2.19996 6.43304 1.74 6.73438 1.365C6.91071 1.1482 7.18304 1.1482 7.35938 1.36793C8.1317 2.33472 8.78572 3.61207 9.24554 4.82496C9.69866 6.02027 10 7.24195 10 8.10328C10 11.8416 7.77232 14.9998 5 14.9998C2.19643 14.9998 0 11.8386 0 8.10035C0 6.97535 0.397321 5.60133 1.01339 4.24195C1.63616 2.86207 2.51563 1.42359 3.5558 0.157967ZM5.03795 12.1873C5.60268 12.1873 6.10268 11.9822 6.57366 11.572C7.51339 10.7107 7.76563 8.98804 7.20089 7.63453C7.13839 7.47047 7.07589 7.3064 6.98214 7.14234L5.85268 8.865C5.85268 8.865 4.03571 5.82984 3.90848 5.62476C2.97098 7.14234 2.5 8.00367 2.5 8.98804C2.5 10.9978 3.62946 12.1873 5.03795 12.1873Z"
@@ -56,10 +64,10 @@ export default {
             />
           </svg>
         </span>
-        <span class="nickName">{{ profileIntroduce.nickName }}</span>
+        <span class="nickName">{{ profileInfo.nickName }}</span>
         <span class="introduce">
-          {{ profileIntroduce.introduce }}
-          <custom-button class="profileEdit">
+          {{ profileInfo.introduce }}
+          <custom-button class="profileEdit" :onClick="() => componentChange(tabType.EDIT)">
             <svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg">
               <path
                 d="M7.11581 13.0483H13.2316"
@@ -94,7 +102,8 @@ export default {
         <div v-else-if="tabIndex === tabType.SELL">'판매'</div>
         <div v-else-if="tabIndex === tabType.RENT">'대여'</div>
         <div v-else-if="tabIndex === tabType.SAVE">'저장'</div>
-        <div v-else>'후기'</div>
+        <div v-else-if="tabIndex === tabType.REVIEW">'후기'</div>
+        <update-profile v-else-if="tabIndex === tabType.EDIT" />
       </div>
     </div>
   </div>
