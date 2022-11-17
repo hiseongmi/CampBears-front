99999b42a394d5d3538a038fee6e13caa57e8923
kimsungmi <rosa6529@naver.com>
2022-11-17 12:10:11 +0900

    used market writing

--- src/components/manager/popups/productWritePopup.vue
+++ src/components/manager/popups/productWritePopup.vue
@@ -2,10 +2,11 @@
 import customButton from "../../layout/customButton.vue";
 import { ref } from "vue";
 import CustomInput from "../../layout/customInput.vue";
+import CustomInputFileButton from "../../layout/customInputFileButton.vue";
 
 export default {
   name: "productWritePopup",
-  components: { CustomInput, customButton },
+  components: { CustomInputFileButton, CustomInput, customButton },
   props: {
     clickClose: {
       type: Function,
@@ -13,6 +14,33 @@ export default {
     },
   },
   setup() {
+    const overall_Type = [
+      {
+        name: "텐트/타프",
+      }, {
+        name: "침낭/매트",
+      }, {
+        name: "테이블/의자",
+      }, {
+        name: "조명 기구",
+      }, {
+        name: "주방 용품",
+      }, {
+        name: "화로/버너",
+      }, {
+        name: "안전/위생용품",
+      }, {
+        name: "전기/전자제품",
+      }, {
+        name: "캠핑카/카라반",
+      }, {
+        name: "차박 용품",
+      }, {
+        name: "의류/수납",
+      }, {
+        name: "기타 용품",
+      },
+    ];
     const typeType = {
       SELL: "sell",
       BUY: "buy",
@@ -21,10 +49,12 @@ export default {
     const checkValue = v => {
       typeIndex.vlaue = v;
     };
+
     return {
       typeType,
       typeIndex,
       checkValue,
+      overall_Type,
     };
   },
 };
@@ -40,7 +70,7 @@ export default {
     <div class="modal-inner-wrap">
       <div class="product">
         <div class="product-file">
-          <input type="file" accept="image/*" />
+          <custom-input-file-button />
         </div>
         <div class="product-title">
           <custom-input :placeholder="'제목'" />
@@ -72,6 +102,22 @@ export default {
             <span><i class="fa-solid fa-star"></i></span>
             <span><i class="fa-solid fa-star"></i></span>
           </div>
+        </div>
+        <div class="product-sort">
+          <div class="product-sort-info">
+            <span>구분</span>
+            <span class="import">*</span>
+          </div>
+          <div class="product-sort-deal">
+            <span>택배거래</span>
+            <span>직거래</span>
+            <div class="product-sort-deal-type">
+              <span v-for="item in overall_Type">{{ item.name }}</span>
+            </div>
+          </div>
+        </div>
+        <div class="product-content">
+          <custom-input :placeholder="'게시글 내용을 작성해주세요. 허위 내용 작성시 게시가 제한될 수 있어요.'" />
         </div>
       </div>
     </div>
