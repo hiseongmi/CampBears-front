0cee5c9814eb82999728e0c8adffe591b2b213cc
calf <81305023+qhaud98@users.noreply.github.com>
2022-12-15 00:32:49 +0900

    캠핑 로그수집 추가

--- src/views/campingDetail.vue
+++ src/views/campingDetail.vue
@@ -1,12 +1,35 @@
 <script>
-import {useStore} from "vuex";
+import { useStore } from "vuex";
+import { onMounted } from "vue";
 
 export default {
   name: "campingDetail",
   setup() {
     const store = useStore();
     //todo 기상청, 카카오지도, 캠핑 세부정보
-    return {}
+
+
+    /**
+     * 유저가 조회한 캠핑 로그 수집
+     * @param campingIdx 캠핑 고유번호
+     * @returns {Promise<void>}
+     */
+    const insertCampingViewLog = async (campingIdx) => {
+      if (campingIdx) {
+        const d = await apiClient("/camping/insertSearchLog", { campingIdx: campingIdx });
+        console.log(d);
+      }
+    };
+
+    onMounted(() => {
+      //todo 주석 살리면됨
+      // if (campInfo && campInfo.campingIdx) {
+      //   insertCampingViewLog(campInfo.campingIdx);
+      // }
+    });
+
+
+    return {};
   }
 };
 </script>
